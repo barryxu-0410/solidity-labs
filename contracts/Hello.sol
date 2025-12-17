964d1eca8f05786e4ae4490a53ddd74455614faa
barryxu-0410 <barryxu2025@gmail.com>
2025-12-17 20:33:28 +0800

    today's learning content

--- contracts/Hello.sol
+++ contracts/Hello.sol
@@ -1,8 +1,31 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.21;
+contract helloweb3{
+    string public _string = "Hello Web3";
+}
 
-contract Hello {
-    function sayHello() public pure returns (string memory) {
-        return "Hello, Solidity";
+contract Function {
+    uint public number = 5;
+    function add(uint _number)public pure  returns(uint newnumber){
+        newnumber=_number+1;
     }
+
+    function returnMuti() public pure returns(uint,bool,uint256[3] memory){
+        return (1,true,[uint256(1),2,4]);
+    }
+}
+
+contract DataStorage{
+    //storge>memory>calldata  #消耗gas
+    function fcalldata(uint[] calldata _x) public pure returns(uint[] calldata){
+        return (_x);
+    }
+
+uint[] x = [1,2,3]; // 状态变量：数组 x
+
+function fStorage() public{
+    //声明一个storage的变量 xStorage，指向x。修改xStorage也会影响x
+    uint[] storage xStorage = x;
+    xStorage[0] = 100;
+}
 }
